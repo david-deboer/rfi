46708b9a38b9ce7b44fe8d96b8624a8f167306d4
Dave D <ddeboer@berkeley.edu>
2020-10-01 07:14:12 -0700

    update program snap args

--- program_snaps.py
+++ program_snaps.py
@@ -8,6 +8,6 @@ with open('CurrentNode.txt', 'r') as fp:
 
 init_file = './hera_snap_feng_init_rfi.py'
 yaml_file = 'hera_feng_rfi_config_node{}.yaml'.format(current_node)
-args = '-p -e -s -i --noredistapcp --nomultithread  --config_file'
+args = '-p -e -s -i --config_file'
 
 os.system('{} {} {}'.format(init_file, args, yaml_file))
